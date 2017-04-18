4c49cac535044db1a75b6d3a1369ecb1fd17932e
amandazhang <amandazhang@berkeley.edu>
2017-04-18 17:16:26 -0400

    add function to get 32 datasets

--- lib/PerGame_data.R
+++ lib/PerGame_data.R
@@ -1,5 +1,5 @@
 data_clean <- function(name, year = "2016") {
-  df <- read.csv(paste0("../data/raw_2016/", name, "_", year, ".csv"), header = T)
+  df <- read.csv(paste0("../data/raw_",year,"/", name, "_", year, ".csv"), header = T)
   df <- df[, -c(1, 25)]
   colnames(df)[7] <- "Opp"
   colnames(df)[24:39] <- gsub(".1", "_opp", colnames(df)[24:39])
@@ -12,13 +12,19 @@ data_clean <- function(name, year = "2016") {
 name_vec <- c("CEL", "CHI", "WAS", "ATL", "TOR", "MIL",  "CLE", "IND", 
               "GSW", "POR", "LAC", "UTA", "HOU", "OKC", "SAS", "MEM")
 m <- sapply(name_vec, data_clean)
+
 for (i in 1:dim(m)[2]) {
   x <- as.data.frame(m[, i])
   write.csv(x, file = paste0("../data/PerGame_2016/", name_vec[i], "_2016.csv"))
   
 }
 
-
+m <- sapply(name_vec, data_clean, year = "2015")
+for (i in 1:dim(m)[2]) {
+  x <- as.data.frame(m[, i])
+  write.csv(x, file = paste0("../data/PerGame_2015/", name_vec[i], "_2015.csv"))
+  
+}
 
 
 
