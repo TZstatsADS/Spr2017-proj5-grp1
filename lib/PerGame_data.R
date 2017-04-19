eb454c8a0291d7abf07b468f05ee73cc276cc17e
amandazhang <amandazhang@berkeley.edu>
2017-04-19 12:20:15 -0400

    add 2015-2016 Playoff data

--- lib/PerGame_data.R
+++ lib/PerGame_data.R
@@ -27,7 +27,23 @@ for (i in 1:dim(m)[2]) {
 }
 
 
+name_playoff <- c("CLE", "TOR", "MIA", "ATL", "CEL", "CHO", "IND", "DET", 
+                  "GSW", "SAS", "OKC", "LAC", "POR", "DAL", "MEM", "HOU")
 
+data_clean <- function(name, year = "2016") {
+  df <- read.csv(paste0("../data/raw_playoff_",year,"/", name, "_", year, ".csv"), header = T)
+  df <- df[, -c(1, 25)]
+  colnames(df)[7] <- "Opp"
+  colnames(df)[24:39] <- gsub(".1", "_opp", colnames(df)[24:39])
+  return(df)
+}
+
+m <- sapply(name_playoff, data_clean)
+for (i in 1:dim(m)[2]) {
+  x <- as.data.frame(m[, i])
+  write.csv(x, file = paste0("../data/PerGame_playoff_2016/", name_vec[i], "_2016.csv"))
+  
+}
 
 
 
