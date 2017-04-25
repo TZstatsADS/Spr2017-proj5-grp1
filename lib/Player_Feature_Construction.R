cf307f4559c30ecb2012418778480f4e05994bca
Ruxue Peng <rp2815@columbia.edu>
2017-04-24 22:12:35 -0400

    Add files via upload

--- lib/Player_Feature_Construction.R
+++ lib/Player_Feature_Construction.R
@@ -7,23 +7,23 @@ source("../lib/core_player.R")
 source("../lib/query_EFF.R")
 source("../lib/loadEFF.R")
 source("../lib/Make_link.R")
+source("../lib/Make_link_playoffs.R")
+
 
 # Load EFF data, data source: http://www.hoopsstats.com/basketball/fantasy/nba/playerstats/16/1/eff/7-1
 # Create a score dictionary for us to query later
 EFFdic = loadEFF()
-##############################################################3
+##############################################################
 # scrap the player lineup of each game
 
 # (1) generate html links
-
-## The link follows pattern: /date + 0 + nameabbreviation +.html
-## eg. http://www.basketball-reference.com/boxscores/201510290NYK.html
-## The name is the team that plays at home.
 # read in game data
 File_list = list.files("../data/PerGame_2015")
 Data = alply(File_list,1,function(path) read.csv(paste0("../data/PerGame_2015/",path),as.is = T))
 links = Make_link(Datatable = Data[[1]],File_list = File_list)
-##############################################################
+#Change to this for playoffs data:
+##links = Make_link_playoffs(Datatable = Data[[1]],I,File_list = File_list)
+#############################################################
 
 # (2) visit the link and store all the html code as txt
 
@@ -46,7 +46,7 @@ MP_hometeam = llply(MP_all_hometeam,coreplayer)
 ##############################################################3
 
 # (4) match lineup data with EFF data
-
+# need to change season = for different game seasons
 EFFScore = ldply(MP,query_EFF,EFFdic,season = "2015/2016regular")
 EFFScore_hometeam = ldply(MP_hometeam,query_EFF,EFFdic,season = "2015/2016regular")
 # deal with NAs
@@ -70,12 +70,14 @@ EFFScore_hometeam$EFFscore[EFFScore_hometeam$Name == "Markieff Morris"] =
 EFFScore = na.omit(EFFScore)
 EFFScore_hometeam = na.omit(EFFScore_hometeam)
 
-##############################################################3
+##############################################################
+
 # (5) output the final Feature matrix
 roadteam_score = ddply(EFFScore,.variables = ".id",function(table) mean(table[,"EFFscore"]))
 hometeam_score = ddply(EFFScore_hometeam,.variables = ".id",function(table) mean(table[,"EFFscore"]))
 EFFscore_20152016regular = cbind(Hometeam = hometeam_score$V1,Roadteam = roadteam_score$V1)
 rownames(EFFscore_20152016regular) = hometeam_score[,1]
+
 # re-arrange according to original data structure
 EFF = ifelse(Data[[1]]$X == "",EFFscore_20152016regular[,1],EFFscore_20152016regular[,2])
 Opp_EFF = ifelse(Data[[1]]$X == "",EFFscore_20152016regular[,2],EFFscore_20152016regular[,1])
@@ -85,5 +87,6 @@ firsthalf = Data[[1]][,1:24]
 firsthalf$EFF = EFF
 secondhalf$EFF_opp = Opp_EFF
 newdata = data.frame(firsthalf,secondhalf)
+
 Filenames = paste0("../data/EFFadded_data/",substr(File_list,1,8),"_EFFadded.csv")
 write.csv(newdata,Filenames[1])
