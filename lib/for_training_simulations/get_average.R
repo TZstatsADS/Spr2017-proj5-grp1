d7ee524df111b4f002f713b5fc7fdefcf264aed0
Jihan Wei <jw3447@columbia.edu>
2017-04-27 11:13:59 -0400

    final_version

--- lib/for_training_simulations/get_average.R
+++ lib/for_training_simulations/get_average.R
@@ -1,10 +1,11 @@
 ##This function is used for getting average historical performance:
 get_average<-function(dataset){
   n<-nrow(dataset)
-  wt<-.99^seq(n, 1, by = -1)
+  wt<-0.99^seq(n, 1, by = -1)
   wt<-wt/sum(wt)
   d<-(ncol(dataset)-2)/2
   weight<-matrix(rep(wt,d+2),nrow = n,byrow = F)
   dataset<-dataset[,c(1:d,ncol(dataset)-1,ncol(dataset))]*weight
   return(colSums(dataset))
 }
+
